614cc0a07b7090810fbd50f477d27d719cb9a002
Andre Silva <andre.beat@gmail.com>
2014-04-11 11:55:42 +0100

    babel support in org-mode

--- modules/prelude-org.el
+++ modules/prelude-org.el
@@ -145,6 +145,39 @@
 (setq org-blank-before-new-entry (quote ((heading)
                                          (plain-list-item . auto))))
 
+;; Babel
+(setq org-plantuml-jar-path "~/.bin/plantuml.jar")
+(add-hook 'org-babel-after-execute-hook 'personal-display-inline-images 'append)
+
+;; Make babel results blocks lowercase
+(setq org-babel-results-keyword "results")
+(defun personal-display-inline-images ()
+  (condition-case nil
+      (org-display-inline-images)
+    (error nil)))
+(org-babel-do-load-languages
+ (quote org-babel-load-languages)
+ (quote ((emacs-lisp . t)
+         (dot . t)
+         (R . t)
+         (python . t)
+         (ruby . t)
+         (gnuplot . t)
+         (clojure . t)
+         (scheme . t)
+         (sh . t)
+         (ledger . t)
+         (org . t)
+         (plantuml . t)
+         (latex . t))))
+
+;; Do not prompt to confirm evaluation
+;; This may be dangerous - make sure you understand the consequences
+;; of setting this -- see the docstring for details
+(setq org-confirm-babel-evaluate nil)
+;; Use fundamental mode when editing plantuml blocks with C-c '
+(add-to-list 'org-src-lang-modes (quote ("plantuml" . fundamental)))
+
 (provide 'prelude-org)
 
 ;;; prelude-org.el ends here
