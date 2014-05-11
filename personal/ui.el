b7905701aa7532a149ab389f46890c8bf6f59059
Andre Silva <andre.beat@gmail.com>
2014-05-12 00:25:33 +0100

    enable desktop mode

--- personal/ui.el
+++ personal/ui.el
@@ -8,5 +8,14 @@
 ;; fallback font for unicode characters
 (set-fontset-font "fontset-default" 'unicode "Menlo-13")
 
+;; enable desktop-mode
+(require 'desktop)
+
+(setq desktop-save t)
+(setq desktop-path (list prelude-savefile-dir))
+
+(setq desktop-dirname prelude-savefile-dir)
+(desktop-save-mode +1)
+
 (require 'nyan-mode)
 ;; (nyan-mode)
