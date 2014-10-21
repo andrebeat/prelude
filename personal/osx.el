97ae8889551ed2bfbb41c03f7435299a536704eb
Andre Silva <andre.beat@gmail.com>
2014-10-21 01:22:01 +0100

    Fullscreen support on emacs 24.4

--- personal/osx.el
+++ personal/osx.el
@@ -1,12 +1,13 @@
 (when (eq system-type 'darwin)
-
-  (prelude-swap-meta-and-super)
-
-  (defun personal-toggle-fullscreen ()
-    "Toggle full screen"
-    (interactive)
-    (set-frame-parameter
-     nil 'fullscreen
-     (when (not (frame-parameter nil 'fullscreen)) 'fullboth)))
-
-  (global-set-key (kbd "<f5>") 'personal-toggle-fullscreen))
+  (if (version< emacs-version "24.4")
+      (progn
+        (defun personal-toggle-fullscreen ()
+          "Toggle full screen"
+          (interactive)
+          (set-frame-parameter
+           nil 'fullscreen
+           (when (not (frame-parameter nil 'fullscreen)) 'fullboth)))
+        (global-set-key (kbd "<f5>") 'personal-toggle-fullscreen))
+    (progn
+      (setq ns-use-native-fullscreen nil)
+      (global-set-key (kbd "<f5>") 'toggle-frame-fullscreen))))
