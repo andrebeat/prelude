ad473cad2bbab1a43dfa4c420d6875048836ddf7
Andre Silva <andre.beat@gmail.com>
2015-06-01 01:10:26 +0100

    enable helm auto resize mode

--- modules/prelude-helm.el
+++ modules/prelude-helm.el
@@ -56,6 +56,8 @@
 (require 'helm-config)
 (require 'helm-projectile)
 
+(helm-autoresize-mode 1)
+
 ;; The default "C-x c" is quite close to "C-x C-c", which quits Emacs.
 ;; Changed to "C-c h". Note: We must set "C-c h" globally, because we
 ;; cannot change `helm-command-prefix-key' once `helm-config' is loaded.
