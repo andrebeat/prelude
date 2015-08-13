a5b75a2f10a2fe258f105ed2c5528d0e39dc86ea
Andre Silva <andre.beat@gmail.com>
2015-08-13 12:25:55 +0100

    company-mode: disable flipping of tooltip

--- modules/prelude-company.el
+++ modules/prelude-company.el
@@ -38,10 +38,6 @@
 (setq company-idle-delay 0.5)
 (setq company-tooltip-limit 10)
 (setq company-minimum-prefix-length 2)
-;; invert the navigation direction if the the completion popup-isearch-match
-;; is displayed on top (happens near the bottom of windows)
-(setq company-tooltip-flip-when-above t)
-
 (setq company-selection-wrap-around t)
 
 (global-company-mode 1)
